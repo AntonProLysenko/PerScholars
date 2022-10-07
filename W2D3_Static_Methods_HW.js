5c42947d5d32d9c604d222bec50f621d0d402889
boldPro <112988428+boldPro@users.noreply.github.com>
2022-10-07 19:15:37 -0400

    I was hungry

--- W2D3_Static_Methods_HW.js
+++ W2D3_Static_Methods_HW.js
@@ -79,25 +79,138 @@ class Chef extends Person{
 
 
 
-const jim = new Person("Jim the person")
+// const jim = new Person("Jim the person")
 
-console.log(jim)
-jim.walk()
+// console.log(jim)
+// jim.walk()
 
-Governor.callPresident()     // here returns class name in console.log, not undefined, Why?
-
-
+// Governor.callPresident()                      // here returns class name in console.log, not undefined, Why?
 
 
-const mark = new PostalWorker('Mark')
-const tim = new PostalWorker("Tim")
-
-console.log(tim);
 
 
-const jakie= new Chef("Jakie")
-const kate = new Chef ("Kate")
+// const mark = new PostalWorker('Mark')
+// const tim = new PostalWorker("Tim")
 
-kate.coockTheMeal()
+// console.log(tim);
 
-tim.giveAnEnvelope()
+
+// const jakie= new Chef("Jakie")
+// const kate = new Chef ("Kate")
+
+// kate.coockTheMeal()
+
+// tim.giveAnEnvelope()
+
+
+
+
+
+//                                                                   ===========I am HUNGRY for More===============
+
+
+
+
+class BankAccount{
+    constructor(namePar, balance, number){
+        this.ownerName = namePar,
+        this.balance = balance,
+        this.accNum = number;
+    }
+    
+        
+    
+
+    deposit(money){
+       let newDeposit =money+this.balance
+        this.balance=newDeposit
+
+    }
+
+    withdraw(bills){
+        if(bills>this.balance){
+
+            console.log('Insuficient Balance');                                  //Here had to disable the overdraft, since by default we can go below 0
+            
+        }else{
+        let newBalance = this.balance-bills
+
+        this.balance=newBalance}
+    }
+    
+}
+
+// const  chase =  new BankAccount( "anton", 20, 1234567890)
+
+// console.log(chase);
+// chase.deposit(20)
+// console.log(chase);
+
+// chase.withdraw(45)
+// console.log(chase);
+
+
+
+class CheckingAccount extends BankAccount{
+    constructor(namePar, balance, number){
+        super(namePar,balance,number)
+        
+    }
+    withdraw(bills){
+        let overdraftEnabled=true
+        if(overdraftEnabled===true){ 
+            let newDeposit =this.balance-bills
+            this.balance=newDeposit
+        }else{
+            if(bills>this.balance){
+
+                console.log('Insuficient Balance');                                  
+                
+            }else{
+            let newBalance = this.balance-bills
+    
+            this.balance=newBalance}
+        }
+    }
+    
+}
+
+
+
+
+// const bofaCheck= new CheckingAccount('Anton', 10, 1234567890)
+
+
+// console.log(bofaCheck);
+// bofaCheck.withdraw(20)
+// console.log(bofaCheck);
+
+
+
+
+
+class SavingsAccount extends BankAccount{
+    constructor(namePar, balance, number){
+        super(namePar,balance,number)
+        
+    }
+    withdraw(bills){
+
+        console.log("No withdraw alowed from your Saving");
+        
+
+        }
+    
+    
+}
+
+
+// const bofaSave= new SavingsAccount('Anton', 300, 1234567890)
+
+
+// console.log(bofaSave);
+// bofaSave.withdraw(20)
+// console.log(bofaSave);
+
+
+

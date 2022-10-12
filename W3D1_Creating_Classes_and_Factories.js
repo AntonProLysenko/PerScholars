e47b7f5f02085668a03f63cfc45e6848f5d1d17b
boldPro <112988428+boldPro@users.noreply.github.com>
2022-10-12 13:18:32 -0400

    created person class

--- W3D1_Creating_Classes_and_Factories.js
+++ W3D1_Creating_Classes_and_Factories.js
@@ -1,8 +1,93 @@
 class Hamster{
-    constructor(){
-        this.owner=""
-        this.name=
-        this.price=
+   
+        owner=""
+        name="Russel"
+        price=15
+    
+
+    wheelRun(){
+
+        console.log("squeak squeak");
+        
+    }
+    eatFood(){
+
+        console.log("nibble nibble");
+        
+    } 
+
+    getPrice() {
+        return  this.price
     }
 
 }
+
+
+
+
+class Person{
+    constructor(name){
+        this.name =name 
+    }
+    age=0
+    height=0
+    weight=0
+    mood =0
+    hamster =[]
+    bankAccount =0
+    
+
+getName() {
+    return this.name
+}
+    
+
+getAge(){
+    return this.age
+}
+
+getWeight(){
+    return this.weight
+
+}
+greet(name){
+    
+    console.log(`Hello! ${name}`);
+    
+}
+eat(){
+    this.weight++
+    this.mood++
+}
+exersise(){
+    this.weight--
+}
+ageUp(){
+    this.age++
+    this.height++
+    this.weight++
+    this.mood--
+    this.bankAccount+=10
+}
+buyHamster(){
+    this.hamster.push(hamster={})
+    this.mood+=10
+    this.bankAccount-=getPrice()
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+const anton = new Person("Anton")
+
+
+console.log(anton);

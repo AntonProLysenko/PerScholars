25e6046f506e78da402ccd218b34c32244b4892c
boldPro <112988428+boldPro@users.noreply.github.com>
2022-10-13 14:32:00 -0400

    Wild Wild Life answered

--- HW_W3D3/answers.js
+++ HW_W3D3/answers.js
@@ -41,11 +41,21 @@ for(let m =0; m<=100; m++){
     }
     
     console.log(m);
-    
-
-
 }
+///////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+plantee[2]=5001
+wolfy.splice(3,1, "Gotham City")
+dart.push("Hawkins")
+wolfy.shift()
+wolfy.unshift( "Gameboy")
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
